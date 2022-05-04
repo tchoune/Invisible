fee8ebb65a4d098209358607f2c19a8e87467db6
Yannick PERRET <yannick.perret@emploilausanne.ch>
2022-05-04 11:19:54 +0200

    Responsive Web v1 + menu onlick

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import './style/_settings.scss'
 
 
 
-    
-window.onload = function (){
-   
+window.showMenuMobile = () =>{
+    const nav = document.querySelector("nav")
+    if(nav.style.display  === "flex"){
+        nav.style.display = "none";
+    }else {
+        nav.style.display = "flex";
+    }
 }
+
