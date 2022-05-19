61864c3dc4c79418c7e4bfb3d9ab68caa060f9cb
Yannick PERRET <yannick.perret@emploilausanne.ch>
2022-05-19 17:09:35 +0200

    travail sur le shover des services

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import './style/_settings.scss'
 import ScrollReveal from 'scrollreveal'
 
+ScrollReveal().reveal('.home-title', {delay : 2000, duration: 3000, opacity: 0.0, viewFactor:0.2 });
+ScrollReveal().reveal('.subTitle', {delay : 2500, duration: 1000, opacity: 0.0, viewFactor:0.2 });
+ScrollReveal().reveal('.about', {duration: 4000, opacity: 0.0, viewFactor:0.2 });
+ScrollReveal().reveal('.reveal-2', { delay: 300, origin: 'left', distance:'150%' });
+ScrollReveal().reveal('.reveal-3',{ delay: 400, origin: 'right', distance:'150%'});
+ScrollReveal().reveal('.reveal-4', { delay: 500, origin: 'bottom',distance:'150%' });
+
 
 
 window.showMenuMobile = () =>{
@@ -21,115 +28,173 @@ window.scrollToForm = (id) => {
 }
 
 
-//ScrollReveal().reveal('.home-title', {delay : 2000, duration: 3000, opacity: 0.0, viewFactor:0.2 });
-//ScrollReveal().reveal('.subTitle', {delay : 2000, duration: 3000, opacity: 0.0, viewFactor:0.2 });
-ScrollReveal().reveal('.about', {duration: 4000, opacity: 0.0, viewFactor:0.2 });
-ScrollReveal().reveal('.reveal-2', { delay: 300, origin: 'left', distance:'150%' });
-ScrollReveal().reveal('.reveal-3',{ delay: 400, origin: 'right', distance:'150%'});
-ScrollReveal().reveal('.reveal-4', { delay: 500, origin: 'bottom',distance:'150%' });
 
  
 window.addEventListener("DOMContentLoaded", () => {
     const oeil = document.querySelector(".onLoad");
-
     oeil.style.display = "flex";
-    document.querySelector("#vOeil").play();
-
     setTimeout(() => {
         oeil.style.display = "none";
-        document.querySelector("#vOeil").pause();
-    }, 2000)
+    }, 2400)
 })
 
 
 
+   
+    /*Text au survol des images*/
+    let onChangeIdentite = true
+    let identite = document.querySelector(".identite2")
+    let oldtextIdentite = identite.innerHTML
+    let nbClickidentite = 1
+    let isShowIdentitie = false
 
-/*Text au survol des images*/
-let onChangeIdentite = true
-let identite = document.querySelector(".identite2")
-let oldtextIdentite = identite.innerHTML
 
+    document.querySelector('.reveal-2').addEventListener("mouseover", () => {
+        if(window.innerWidth > 900 && !isShowIdentitie) {
+            isShowIdentitie = true
+            document.querySelector('.identite_cards__text').style.display = 'flex';
+        }
+    })
 
-document.querySelector('.reveal-2').addEventListener("mouseover", () => {
-
-    console.log("test1")
-    document.querySelector('.reveal-2 > .services__content__text').style.display = 'flex';
+    document.querySelector('.reveal-2').addEventListener("mouseleave", () => {
+        if(window.innerWidth > 900 && isShowIdentitie) {
+            isShowIdentitie = false
+            document.querySelector('.identite_cards__text').style.display = 'none';
+        }
+    })
 
     document.querySelector('.reveal-2').addEventListener("click", () => {
-
-        console.log("test2")
-        if(onChangeIdentite){
+        if (nbClickidentite === 2){
+    
             identite.innerHTML = oldtextIdentite
-            }else{
+            document.querySelector('.identite_cards__text').style.display = 'none';
+            nbClickidentite = 0;
+        }
+
+        else{
+
+            document.querySelector('.identite_cards__text').style.display = 'flex';
+            if (nbClickidentite === 0){
+                console.log("TEst3", nbClickidentite)
+
+                identite.innerHTML = oldtextIdentite
+            }
+    
+            else if (nbClickidentite === 1){
+                console.log("TEst4", nbClickidentite)
+
                 identite.innerHTML ="<ul><li>MODÉLISATION 3D</li><li>MOTION DESIGN</li> <li>DÉVELOPPEMENT WEB</li> <li>WEB DESIGN</li> <li>CAMPAGNE DIGITALE</li><li>RÉSEAUX SOCIAUX</li></ul>"
             }
-            onChangeIdentite = !onChangeIdentite
-        
-        })
-})
+            nbClickidentite++;
+            console.log(nbClickidentite)
+        }
+        onChangeIdentite = !onChangeIdentite
+    })
 
 
-
-document.querySelector('.reveal-2').addEventListener("mouseleave", () => {
-    console.log("test3")
-    document.querySelector('.services__content__text').style.display = 'none';
-
-    identite.innerHTML = oldtextIdentite
-    onChangeIdentite = false
-})
+   
 
 
+    let onChangePrint = true
+    let print = document.querySelector(".print2")
+    let oldtextPrint = print.innerHTML
+    let nbClickPrint = 0
+    let isShowPrint = false
 
 
-
-
-
-let onChangePrint = true
-let print = document.querySelector(".print2")
-let oldtextPrint = print.innerHTML
-
-document.querySelector('.reveal-2').addEventListener("mouseover", () => {
-
-    console.log("test1")
-    document.querySelector('.reveal-2 > .services__content__text').style.display = 'flex';
+    document.querySelector('.reveal-3').addEventListener("mouseover", () => {
+        if(window.innerWidth > 900 && !isShowPrint) {
+            isShowPrint = true
+            document.querySelector('.print_cards__text').style.display = 'flex';
+        }
+    })
+    document.querySelector('.reveal-3').addEventListener("mouseleave", () => {
+        if(window.innerWidth > 900 && isShowPrint) {
+            isShowPrint = false
+            document.querySelector('.print_cards__text').style.display = 'none';
+        }
+    })
 
     document.querySelector('.reveal-3').addEventListener("click", () => {
-        if(onChangePrint){
-            print.innerHTML = oldtextPrint
-        }else{
-            print.innerHTML ="<ul><li>CARTE DE VISITE</li><li>FLYERS</li> <li>PAPIER À LETTRE</li> <li>GOODIES</li> <li>BÂCHE PUBLICITAIRE</li></ul>"
+
+
+        if (nbClickPrint === 2){
+    
+            identite.innerHTML = oldtextPrint
+            document.querySelector('.print_cards__text').style.display = 'none';
+            nbClickPrint = 1;
         }
-        onChangePrint = !onChangePrint
 
+        else{
+
+            document.querySelector('.print_cards__text').style.display = 'flex';
+            if (nbClickPrint === 0){
+
+                print.innerHTML = oldtextPrint
+            }
+    
+            else if (nbClickPrint === 1){
+                print.innerHTML ="<ul><li>CARTE DE VISITE</li><li>FLYERS</li> <li>PAPIER À LETTRE</li> <li>GOODIES</li> <li>BÂCHE PUBLICITAIRE</li></ul>"
+            }
+            nbClickPrint++;
+        }
     })
-})
-
-document.querySelector('.reveal-3').addEventListener("mouseleave", () => {
-    document.querySelector('.services__content__text').style.display = 'none';
-
-    print.innerHTML = oldtextPrint
-    onChangePrint = false
-})
 
 
 
 
-let onChangeDigital = true
-let digital = document.querySelector(".digital2")
-let oldtextDigital = digital.innerHTML
 
-document.querySelector('.reveal-4').addEventListener("click", () => {   
-    if(onChangeDigital){
+
+
+
+
+
+    let onChangeDigital = true
+    let digital = document.querySelector(".digital2")
+    let oldtextDigital = digital.innerHTML
+    let nbClickDigital = 0
+    let isShowDigital = false
+
+
+    document.querySelector('.reveal-4').addEventListener("mouseover", () => {
+        if(window.innerWidth > 900) {
+            digital.innerHTML = onChangeDigital
+            document.querySelector('.digital_cards__text').style.display = 'flex';
+            onChangeDigital = false
+
+        }
+    })
+
+
+
+
+    document.querySelector('.reveal-4').addEventListener("click", () => {   
+         if( nbClickDigital === 2){
+            document.querySelector('.digital_cards__text').style.display = 'none';
+            nbClickDigital = 0;
+        }
+        else{
+            if(onChangeDigital){
+                document.querySelector('.digital_cards__text').style.display = 'flex';
+                digital.innerHTML = oldtextDigital
+    
+            }else{
+                document.querySelector('.digital_cards__text').style.display = 'flex';
+                digital.innerHTML ="<ul><li>LOGO</li><li>CHARTE GRAPHIQUE</li> <li>STRATÉGIE MARKETING</li> <li>STORY-TELLING</li></ul>" 
+            }
+            nbClickDigital++;
+        }
+        
+        onChangeDigital = !onChangeDigital
+    })
+
+    document.querySelector('.reveal-4').addEventListener("mouseleave", () => {
+
+        document.querySelector('.digital_cards__text').style.display = 'none';
+
         digital.innerHTML = oldtextDigital
-    }else{
-        digital.innerHTML ="<ul><li>LOGO</li><li>CHARTE GRAPHIQUE</li> <li>STRATÉGIE MARKETING</li> <li>STORY-TELLING</li></ul>" 
-    }
-    onChangeDigital = !onChangeDigital
-})
+        onChangeDigital = false
+    })
 
-document.querySelector('.reveal-4').addEventListener("mouseleave", () => {
-    digital.innerHTML = oldtextDigital
-    onChangeDigital = false
-})
 
 
